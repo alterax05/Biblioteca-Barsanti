2eb2be99b1c8a3461c0349a0442bce8e15cec03b
alterax05 <107863063+alterax05@users.noreply.github.com>
2024-01-05 20:43:41 +0100

    Refactor Vue methods

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,89 +1,86 @@
 new Vue({
-    el: '#main',
-    data: {
-        libri: [],
-        image: "",
-        prenota: null,
-        copia: 0,
-        restituisci: null,
+  el: "#main",
+  data: {
+    libri: [],
+    image: "",
+    prenota: null,
+    copia: 0,
+    restituisci: null,
+  },
+  methods: {
+    update: function (event) {
+      let value = event.target.value;
+      if (value !== "") {
+        axios
+          .get({
+            url: "/api/autori/" + value.replace(" ", "-"),
+          })
+          .then((response) => {
+            this.libri = response.data;
+          });
+      } else {
+        this.libri = [];
+      }
     },
-    methods: {
-        update: function(event) {
-            let value = event.target.value;
+    changeValue: function (value) {
+      $("#autore").val(value);
+      this.libri = [];
+    },
+    rest: function (event) {
+      let value = event.target.value;
 
-            if(value !== '') {
-                ;(async () => {
-                    const response = await axios({
-                        url: '/api/autori/' + value.replace(' ', '-'),
-                        method: 'get'
-                    })
-                    this.libri = response.data;
-                })()
-            }else{
-                this.libri = [];
-            }
-        },
-        changeValue: function(value) {
-            $('#autore').val(value);
-            this.libri = [];
-        },
-        rest: function(event) {
-            let value = event.target.value;
-
-            if(value.length === 13) {
-                axios({
-                    url: '/api/admin/restituisci/' + value,
-                    method: 'get'
-                })
-                .then(response => {
-                    if(response.data !== undefined) {
-                        this.restituisci = response.data;
-                    } else {
-                        this.restituisci = null;
-                    }
-                })
-                .catch(error => {
-                    console.error(error);
-                    this.restituisci = null;
-                });
-
+      if (value.length === 13) {
+        axios
+          .get({
+            url: "/api/admin/restituisci/" + value,
+          })
+          .then((response) => {
+            if (response.data !== undefined) {
+              this.restituisci = response.data;
             } else {
-                this.restituisci = null;
+              this.restituisci = null;
             }
-        },
-        scannerISBN: function(event) {
-            let value = event.target.value;
-
-            if(value.length === 13) {
-                this.image = "https://pictures.abebooks.com/isbn/" + value + "-us-300.jpg"
-
-                ;(async () => {
-                    const response = await axios({
-                        url: '/api/admin/search/' + value,
-                        method: 'get'
-                    })
-
-                    if(response.data !== undefined) {
-                        this.prenota = response.data
-                    }else{
-                        this.prenota = null
-                    }
-                })()
-
-            }else{
-                this.image = ""
-                this.prenota = null
-            }
-        },
-        selectCopia(event) {
-            this.copia = $("#copiaselect option:selected").index()
-        }
+          })
+          .catch((error) => {
+            console.error(error);
+            this.restituisci = null;
+          });
+      } else {
+        this.restituisci = null;
+      }
     },
-})
+    scannerISBN: function (event) {
+      let value = event.target.value;
+
+      if (value.length === 13) {
+        this.image =
+          "https://pictures.abebooks.com/isbn/" + value + "-us-300.jpg";
+        axios
+          .get({
+            url: "/api/admin/search/" + value,
+          })
+          .then((response) => {
+            response.data !== undefined
+              ? (this.prenota = response.data)
+              : (this.prenota = null);
+          })
+          .catch((error) => {
+            console.error(error);
+            this.prenota = null;
+          });
+      } else {
+        this.image = "";
+        this.prenota = null;
+      }
+    },
+    selectCopia(event) {
+      this.copia = $("#copiaselect option:selected").index();
+    },
+  },
+});
 
 function imgError(image) {
-    image.onerror = "";
-    image.src = "/imgs/notcover-min.png";
-    return true;
+  image.onerror = "";
+  image.src = "/imgs/notcover-min.png";
+  return true;
 }
-
--- public/js/book.js
+++ public/js/book.js
@@ -1,70 +1,67 @@
 new Vue({
-    el: '#main',
-    data: {
+  el: "#main",
+  data: {},
+  methods: {
+    recensioni: function (recensione) {
+      axios
+        .post({
+          url: "/api/recensioni/",
+          headers: {
+            "X-CSRF-TOKEN": $("meta[name=csrf-token]").attr("content"),
+            "X-Requested-With": "XMLHttpRequest",
+          },
+          data: {
+            ISBN: $('input[name="ISBN"]').val(),
+            punteggio: recensione,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            $(".toast").toast("show");
+            $(".toast .toast-body").html("Recensione salvata con successo!");
+            var i = 1;
+            $(".rec").each(function () {
+              if (i > recensione) $(this).addClass("removed");
 
+              if (i <= recensione) $(this).removeClass("removed");
+
+              i++;
+            });
+          }
+        });
     },
-    methods: {
-        recensioni: function (recensione) {
-            ;(async () => {
-                const response = await axios({
-                    url: '/api/recensioni/',
-                    method: 'post',
-                    headers: {
-                        'X-CSRF-TOKEN' : $('meta[name=csrf-token]').attr('content'),
-                        'X-Requested-With': 'XMLHttpRequest',
-                    },
-                    data: {
-                        'ISBN': $('input[name="ISBN"]').val(),
-                        'punteggio': recensione,
-                    }
-                })
-
-                if(response.status === 200) {
-                    $('.toast').toast('show')
-                    $('.toast .toast-body').html("Recensione salvata con successo!")
-                    var i = 1;
-                    $(".rec").each(function() {
-                        if(i > recensione)
-                            $(this).addClass("removed");
-
-                        if(i <= recensione)
-                            $(this).removeClass("removed");
-
-                        i++;
-                    });
-                }
-            })()
-        },
-        prenotazione: function () {
-            ;(async () => {
-                const response = await axios({
-                    url: '/api/prenotazione',
-                    method: 'post',
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content'),
-                        'X-Requested-With': 'XMLHttpRequest',
-                    },
-                    data: {
-                        'id_copia': $('select[name="id_copia"]').val(),
-                    }
-                })
-                if(response.status === 200) {
-                    if(response.data === "Prenotazione salvata!") {
-                        $('#prenota').modal('hide')
-                        $('.toast').toast({delay: 5000})
-                        $('.toast').toast('show')
-                        $('.toast .toast-body').html("Prenotazione salvata con successo!")
-                    }else{
-                        $("#prenota .error").html(response.data)
-                    }
-                }
-            })()
-        }
-    }
+    prenotazione: function () {
+      axios
+        .post({
+          url: "/api/prenotazione",
+          headers: {
+            "X-CSRF-TOKEN": $("meta[name=csrf-token]").attr("content"),
+            "X-Requested-With": "XMLHttpRequest",
+          },
+          data: {
+            id_copia: $('select[name="id_copia"]').val(),
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            if (response.data === "Prenotazione salvata!") {
+              $("#prenota").modal("hide");
+              $(".toast").toast({ delay: 5000 });
+              $(".toast").toast("show");
+              $(".toast .toast-body").html(
+                "Prenotazione salvata con successo!"
+              );
+            } else {
+              $("#prenota .error").html(response.data);
+            }
+          }
+        });
+    },
+  },
 });
 
 function imgError(image) {
-    image.onerror = "";
-    image.src = "/imgs/notcover.png";
-    return true;
+  image.onerror = "";
+  image.src = "/imgs/notcover.png";
+  return true;
 }
