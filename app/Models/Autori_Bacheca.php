36b659c2ea904061e73f2de524425af7861f69b0
alterax05 <107863063+alterax05@users.noreply.github.com>
2024-01-06 14:26:32 +0100

    Refactor code to improve performance and readability Suggested using pivot table using the eloquent model

---
+++ app/Models/Autori_Bacheca.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Autori_Bacheca extends Model
+{
+    protected $table = 'autori_bacheca';
+
+    protected $primaryKey = 'id_autore';
+
+    public $incrementing = false;
+
+    protected $keyType = 'bigint';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'outdated_at';
+
+    protected $fillable = [
+        'id_autore',
+        'subtitle',
+        'created_at',
+        'outdated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+}
